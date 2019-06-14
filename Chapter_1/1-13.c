c7ea477a93b3154faaed93c50f7923a47b0dd528
Allen <allen.j.blount@gmail.com>
2019-06-14 14:56:14 -0400

    Initial commit

--- Chapter_1/1-13.c
+++ Chapter_1/1-13.c
@@ -12,5 +12,36 @@ Prints histogram of word count*/
 
 int main(void)
 {
+	int c, state;
+	int nc = 0;					// new character counting
+	int ndata[MAXLETTER];		// Array to hold data of letter count
+
+	state = OUT;
+
+	for (int i = 0; i < 10; ++i)
+		ndata[i] = 0;
+
+	while ((c = getchar()) != EOF){
+		if (c != ' ' && c != '\n' && '\t'){
+			state = IN;
+			++nc;
+		}
+		else if (state == IN){
+			state = OUT;
+			++ndata[nc - 1];
+			nc = 0;
+		}
+	}
+
+	for (int i = 0; i < 10; ++i){
+		printf("|%d|", i+1);
+		if (ndata[i] > 0){
+			for (int n = 0; n < ndata[i]; ++n)
+				printf("*");
+			printf("\n");
+		} else
+				printf("\n");
+	}
+
 	return 0;
 }
